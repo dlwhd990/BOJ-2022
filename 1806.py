8c8461cab2fed3aef5652e1373ba4147b47ff89e
JONGHYUK LEE <dlwhd990@gmail.com>
2022-02-01 01:15:46 +0900

    first commit

---
+++ 1806.py
@@ -0,0 +1,30 @@
+import sys
+I = sys.stdin.readline
+n,m = map(int,I().split())
+list = list(map(int,I().split()))
+
+left = 0
+right = 0
+value = list[0]
+result = int(1e10)
+
+while left <= right:
+    if left == right and value >= m:
+        result = 1
+        break
+
+    elif value >= m:
+        result = min(result,right-left+1)
+        value -= list[left]
+        left += 1
+
+    elif value < m:
+        if right == n-1:
+            break
+        right += 1
+        value += list[right]
+
+if result == int(1e10):
+    print(0)
+else:
+    print(result)
